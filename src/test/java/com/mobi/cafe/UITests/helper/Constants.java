b2fffadd39065b777c47df01bec1cbcfd7aa247c
ahmet-izgi <ahmetizgi@gmail.com>
2019-08-27 09:28:33 +0300

    startup link changed

--- src/test/java/com/mobi/cafe/UITests/helper/Constants.java
+++ src/test/java/com/mobi/cafe/UITests/helper/Constants.java
@@ -3,9 +3,9 @@ package com.mobi.cafe.UITests.helper;
 public class Constants {
 	// this constants also should be read from a property file.
 	public final static String EMPLOYEE_CREATION_ERROR = "Error trying to create a new employee";
-	public final static String STARTUP_URL = "http://cafetownsend-angular-rails.herokuapp.com";
-	public final static long PAGE_LOAD_TIMEOUT = 5;
-	public final static long IMPLICIT_WAIT = 10;
+	public final static String STARTUP_URL = "http://cafetownsend-angular-rails.herokuapp.com/login";
+	public final static long PAGE_LOAD_TIMEOUT = 15;
+	public final static long IMPLICIT_WAIT = 15;
 
 	public enum CssClasses {
 		DISABLED("disabled");
